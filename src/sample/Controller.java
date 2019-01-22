d181400bc64f6cff915ee805f570687ac56da827
Krzysztof Świerkowski <swierkowski.k@gmail.com>
2019-01-22 22:28:32 +0100

    implement calculations

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,10 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 
+import java.util.Locale;
+
+import static java.util.Locale.*;
+
 public class Controller {
 
 
@@ -11,12 +15,27 @@ public class Controller {
     public TextField BMI;
     public TextField index;
 
+    public void disableField()
+    {
+        BMI.setDisable(true);
+        index.setDisable(true);
+    }
 
     public void calculateButton (ActionEvent e) {
 
-        double bmi = (Integer.parseInt (mass.getText ())/ Math.pow ((Integer.parseInt ( height.getText ())),2 ));
+        double bmi = (Double.valueOf(mass.getText ())/ Math.pow ((Double.valueOf ( height.getText ())),2 ));
 
-        BMI.setText ( String.valueOf ( bmi ) );
+        BMI.setText ( String.format ("%.2f",bmi) );
+
+        if(bmi < 18.4)
+            index.setText("niedowaga");
+        else if (18.5 < bmi && bmi < 24.9)
+        index.setText("waga prawidłowa");
+        else if  (25 < bmi && bmi < 29.9)
+            index.setText("nadwaga");
+        else if  (30 > bmi)
+            index.setText("otyłość");
+
 
 
     }
